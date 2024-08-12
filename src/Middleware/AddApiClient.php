f59d7a69b21c2d88c8e4cc86a27bf28f1ea33422
Guba Norbert <guba.norbert@unas.hu>
2024-08-12 15:58:13 +0200

    Minor changes

--- src/Middleware/AddApiClient.php
+++ src/Middleware/AddApiClient.php
@@ -16,7 +16,6 @@ class AddApiClient extends Middleware implements MiddlewareInterface
 {
     public function process(Request $request, RequestHandler $handler): ResponseInterface
     {
-        error_log('>>>>>>>>>>>>>>>>>>' . json_encode($_SESSION));
         $storage = new Storage($this->container->get('app-root'), $_SESSION['shop_id']);
         $this->container->set('storage', $storage);
         $this->container->set('unas-api', new Client([
